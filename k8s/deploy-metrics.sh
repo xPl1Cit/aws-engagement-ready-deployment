7c0f37c6e147c8343f5fbf42323eb6ca5fee4e36
Andreas Lorenz <info@andreaslorenz.tech>
2025-07-15 00:55:16 +0200

    Update

--- k8s/deploy-metrics.sh
+++ k8s/deploy-metrics.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Add and update Helm repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack -f k8s/deployment/prometheus-config.yaml
+
+# Check if the Helm release already exists
+if helm status prometheus >/dev/null 2>&1; then
+    echo "✅ Helm release 'prometheus' already exists. Skipping installation."
+else
+    echo "🚀 Installing Prometheus stack..."
+    helm install prometheus prometheus-community/kube-prometheus-stack -f k8s/deployment/prometheus-config.yaml
+    echo "✅ Prometheus stack installed successfully."
+fi
