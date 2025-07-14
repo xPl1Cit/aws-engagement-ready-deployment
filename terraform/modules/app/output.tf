760543117bd2a3545fa83ae68daf818c637983f1
Andreas Lorenz <info@andreaslorenz.tech>
2025-07-14 13:55:05 +0200

    Update

--- terraform/modules/app/output.tf
+++ terraform/modules/app/output.tf
@@ -1,17 +1,17 @@
 output "rds_endpoint" {
-  value = database.rds_endpoint
+  value = module.database.rds_endpoint
   description = "The RDS Postgres endpoint for the Product DB"
 }
 
 output "rds_db_name" {
-  value = database.rds_db_name
+  value = module.database.rds_db_name
 }
 
 output "rds_username" {
-  value = database.rds_username
+  value = module.database.rds_username
 }
 
 output "rds_password" {
-  value     = database.rds_password
+  value     = module.database.rds_password
   sensitive = true
 }
--- terraform/stages/dev/output.tf
+++ terraform/stages/dev/output.tf
@@ -1,16 +1,16 @@
 output "rds_endpoint" {
-  value = app.rds_endpoint
+  value = module.app.rds_endpoint
 }
 
 output "rds_db_name" {
-  value = app.rds_db_name
+  value = module.app.rds_db_name
 }
 
 output "rds_username" {
-  value = app.rds_username
+  value = module.app.rds_username
 }
 
 output "rds_password" {
-  value     = app.rds_password
+  value     = module.app.rds_password
   sensitive = true
 }
