c8f77e7175d645c5b9bb36d1d3b499f0be6b1037
Andreas Lorenz <info@andreaslorenz.tech>
2025-07-15 00:49:23 +0200

    Fix Path

--- k8s/deploy-metrics.sh
+++ k8s/deploy-metrics.sh
@@ -2,4 +2,4 @@
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack -f deployment/prometheus-config.yaml
+helm install prometheus prometheus-community/kube-prometheus-stack -f k8s/deployment/prometheus-config.yaml
