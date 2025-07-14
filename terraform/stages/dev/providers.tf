d82e20927630529b0f478134d9f8c4b56087c7ef
Andreas Lorenz <info@andreaslorenz.tech>
2025-07-14 14:20:04 +0200

    Implemented s3 remote

--- terraform/stages/dev/providers.tf
+++ terraform/stages/dev/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.95.0"
     }
   }
+
+  backend "s3" {
+    bucket = "my-terraform-state"
+    key    = "eks/cluster/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
 }
 
 provider "aws" {
